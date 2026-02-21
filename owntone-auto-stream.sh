1a8a1cd5a66888e70f9d42aa3cdba2995b45aca4
Julius van der Vaart <julius@vanderva.art>
2026-02-21 14:52:09 +0100

    db output from sox

--- owntone-auto-stream.sh
+++ owntone-auto-stream.sh
@@ -88,7 +88,7 @@ start_monitor() {
 
     log "Starting monitor process..."
     (
-        sox -t alsa "$MONITOR_DEV" -n stats -w "$sample_duration" 2>&1 \
+        SOX_OPTS= sox -V1 -t alsa "$MONITOR_DEV" -n stats -w "$sample_duration" 2>&1 \
             | tr '\r' '\n' \
             | tee -a "$MONITOR_LOG" \
             | awk '/RMS/ && /dB/ {
