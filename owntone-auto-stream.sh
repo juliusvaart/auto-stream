c806e66fc8bc433d061184b1a79b46584cc6cc07
Julius van der Vaart <julius@vanderva.art>
2026-02-21 15:03:01 +0100

    Change order of api play and output command

--- owntone-auto-stream.sh
+++ owntone-auto-stream.sh
@@ -81,9 +81,8 @@ start_stream() {
             | jq -r --arg n "$OUTPUT_NAME" '.outputs[] | select(.name==$n) | .id' \
             | head -n1)
 
-        curl -s -X PUT "$OWNTONE_BASE_URL/api/outputs/set" --data "{\"outputs\":[\"$ID\"]}" >/dev/null
-        sleep 1
         curl -s -X POST "$OWNTONE_BASE_URL/api/queue/items/add?clear=true&playback=start&uris=$OWNTONE_STREAM_URI" >/dev/null
+        curl -s -X PUT "$OWNTONE_BASE_URL/api/outputs/set" --data "{\"outputs\":[\"$ID\"]}" >/dev/null
 
         if [ -n "${OWNTONE_VOLUME:-}" ]; then
             curl -s -X PUT "$OWNTONE_BASE_URL/api/player/volume?volume=$OWNTONE_VOLUME" >/dev/null
